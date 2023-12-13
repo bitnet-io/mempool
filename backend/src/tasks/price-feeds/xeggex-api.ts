7c4031e1d50f958b57cf25e7814cafadf775cc2e
0xcircuitbreaker <sarris88p@gmail.com>
2023-12-13 14:43:42 -0500

    xeggex api fix

--- backend/src/tasks/price-feeds/xeggex-api.ts
+++ backend/src/tasks/price-feeds/xeggex-api.ts
@@ -1,6 +1,19 @@
 import { query } from '../../utils/axios-query';
 import priceUpdater, { PriceFeed, PriceHistory } from '../price-updater';
 
+ interface CandleData {
+  time: number;
+  close: number;
+  open: number;
+  high: number;
+  low: number;
+  volume: number;
+}
+
+interface CandleApiResponse {
+  bars: CandleData[];
+}
+
 class XeggexApi implements PriceFeed {
   public name: string = 'Xeggex';
   public currencies: string[] = ['USDT'];
@@ -12,38 +25,43 @@ class XeggexApi implements PriceFeed {
   }
 
   public async $fetchPrice(currency): Promise<number> {
-    const response = await query(this.url + currency);
+    const response = await query(this.url + encodeURIComponent(currency));
 //    if (response && response['last']) {
     if (response && response['lastPrice']) {
 //      return parseInt(response['last'], 10);
-      return parseInt(response['lastPrice'], 10);
+      return parseFloat(response['lastPrice']);
     } else {
       return -1;
     }
-  }
+  } 
 
   public async $fetchRecentPrice(currencies: string[], type: 'hour' | 'day'): Promise<PriceHistory> {
     const priceHistory: PriceHistory = {};
-
+  
     for (const currency of currencies) {
       if (this.currencies.includes(currency) === false) {
         continue;
       }
+  
+      const resolution = type === 'hour' ? '15' : '1440'; // '15' for 15 minutes and '1440' for 1 day
+      const symbol = encodeURIComponent(currency);
+      const countBack = 15;
+      const firstDataRequest = 1;
+  
+      const url = `https://api.xeggex.com/api/v2/market/candles?symbol=${symbol}&resolution=${resolution}&countBack=${countBack}&firstDataRequest=${firstDataRequest}`;
+      const response = await query(url) as CandleApiResponse;
 
-      const response = await query(this.urlHist.replace('{GRANULARITY}', type === 'hour' ? '1hr' : '1day').replace('{CURRENCY}', currency));
-      const pricesRaw = response ? response : [];
-
-
-
-for (const price of pricesRaw as any[]) {
-        const time = Math.round(price[0] / 1000);
-        if (priceHistory[time] === undefined) {
-          priceHistory[time] = priceUpdater.getEmptyPricesObj();
+      if (response && response.bars) {
+        for (const bar of response.bars) {
+          const time = Math.round(bar.time / 1000);
+          if (priceHistory[time] === undefined) {
+            priceHistory[time] = priceUpdater.getEmptyPricesObj();
+          }
+          priceHistory[time][currency] = bar.close; // Using the 'close' price
         }
-        priceHistory[time][currency] = price[4];
       }
     }
-
+  
     return priceHistory;
   }
 }
